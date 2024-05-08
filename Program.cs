a7408cea8379469283baf331127a7adbec5d12de
Ravi Edara <Ravi.Edara@apmgroup.co.uk>
2024-05-09 00:10:30 +0100

    code commented

--- Program.cs
+++ Program.cs
@@ -2,24 +2,27 @@
 using Azure.AI.OpenAI.Assistants;
 
 // OpenAI API
-var client = new AssistantsClient("sk-proj-I7b5MP8U5zShn9mXEIVaT3BlbkFJpjHzCFzuBd52FZZHsYIe");
+var client = new AssistantsClient("sk-proj-YdwHpSlDdXbIQAc5BtzMT3BlbkFJRQtcX0Qrj65JcwM6aqFo");
 
-// For Azure OpenAI service the model name is the "deployment" name
-var assistantCreationOptions = new AssistantCreationOptions("gpt-4-turbo")
-{
-    Name = "Chat with PDF",
-    Instructions = "Extract the information from the uploaded PDF file. ",
-    Tools = { new CodeInterpreterToolDefinition() },
-};
+/***** Commented to use hard coded id's which I already made matured enough to query for. ****/
+//var assistantCreationOptions = new AssistantCreationOptions("gpt-4-turbo")
+//{
+//    Name = "Chat with PDF",
+//    Instructions = "Extract the information from the uploaded PDF file. ",
+//    Tools = { new CodeInterpreterToolDefinition() },
+//};
 
-var fileName = "../../../Models/fan-manual.pdf";
+//var fileName = "../../../Models/fan-manual.pdf";
 
-var fileUploadResponse = await client.UploadFileAsync(fileName, OpenAIFilePurpose.Assistants);
-assistantCreationOptions.FileIds.Add(fileUploadResponse.Value.Id);
-Console.WriteLine($"Uploaded file {fileUploadResponse.Value.Filename}");
+//var fileUploadResponse = await client.UploadFileAsync(fileName, OpenAIFilePurpose.Assistants);
+//assistantCreationOptions.FileIds.Add(fileUploadResponse.Value.Id);
+//Console.WriteLine($"Uploaded file {fileUploadResponse.Value.Filename}");
 
-var assistant = await client.CreateAssistantAsync(assistantCreationOptions);
-var thread = await client.CreateThreadAsync();
+//var assistant = await client.CreateAssistantAsync(assistantCreationOptions);
+//var thread = await client.CreateThreadAsync();
+
+var assistantId = "asst_LFZ4HWeXKIOV0YNzZOY5Sbq6";
+var threadId = "thread_YFhOqJ5iT88PUaP1if0DQhYh";
 
 Console.WriteLine("Ask a question about the file (empty response to quit):");
 var question = Console.ReadLine();
@@ -28,21 +31,21 @@ while (!string.IsNullOrWhiteSpace(question))
 {
     string? lastMessageId = null;
 
-    await client.CreateMessageAsync(thread.Value.Id, MessageRole.User, question);
-    var run = await client.CreateRunAsync(thread.Value.Id, new CreateRunOptions(assistant.Value.Id));
+    await client.CreateMessageAsync(threadId /*thread.Value.Id*/, MessageRole.User, question);
+    var run = await client.CreateRunAsync(threadId /*thread.Value.Id*/, new CreateRunOptions(assistantId /*assistant.Value.Id*/));
     Response<ThreadRun> runResponse;
 
     do
     {
         await Task.Delay(TimeSpan.FromMilliseconds(1000));
-        runResponse = await client.GetRunAsync(thread.Value.Id, run.Value.Id);
+        runResponse = await client.GetRunAsync(threadId /*thread.Value.Id*/, run.Value.Id);
         Console.Write($".");
     } while (runResponse.Value.Status == RunStatus.Queued
             || runResponse.Value.Status == RunStatus.InProgress);
 
     Console.WriteLine(string.Empty);
 
-    var messageResponse = await client.GetMessagesAsync(thread.Value.Id, order: ListSortOrder.Ascending, after: lastMessageId);
+    var messageResponse = await client.GetMessagesAsync(threadId /*thread.Value.Id*/, order: ListSortOrder.Ascending, after: lastMessageId);
     var found = false;
 
     foreach (var message in messageResponse.Value.Data)
@@ -74,6 +77,6 @@ while (!string.IsNullOrWhiteSpace(question))
 
 // clean up the file, thread and assistant
 Console.WriteLine("Cleaning up and exiting...");
-await client.DeleteFileAsync(fileUploadResponse.Value.Id);
-await client.DeleteThreadAsync(thread.Value.Id);
-await client.DeleteAssistantAsync(assistant.Value.Id);
+//await client.DeleteFileAsync(fileUploadResponse.Value.Id);
+await client.DeleteThreadAsync(threadId /*thread.Value.Id*/);
+await client.DeleteAssistantAsync(assistantId);
